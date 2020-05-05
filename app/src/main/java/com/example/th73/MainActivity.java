e81620dba425f5b2003c3aa0f8cfdbefe79b9f25
ellaqq3 <elpoo@windowslive.con>
2020-05-05 13:04:18 +0300

    Initial commit

---
+++ app/src/main/java/com/example/th73/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.th73;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText name;
+    TextView text;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        text = (TextView) findViewById(R.id.textView);
+        name = (EditText) findViewById(R.id.editText);
+
+    }
+
+    public void pushButton(View v){
+        text.setText(name.getText());
+    }
+}
